460ffe41fed4acee2060d997730770a654aadb9a
Wilson Goltara <wilson.goltara15@gmail.com>
2021-03-04 23:59:26 -0300

    deleted arquives

--- ED3-v1.c
+++ ED3-v1.c
@@ -21,32 +21,30 @@ int main() {
     printf("4- Euro(EUR)    || R$%.2f\n", eur);
     printf("5- Iene(JPY)    || R$%.2f\n", jpy);
     // PROCESSAMENTO
-    printf("Digite o valor a ser convertido: ");
+    printf("Digite o valor a ser convertido:R$ ");
     scanf("%f", &valor_usu);
     printf("Moeda para conversão (numero da tabela): ");
     scanf("%d", &moeda);
 
     switch(moeda) {                                                                        /*No segundo momento da função principal, usei o "switch - case" para já imprimir*/
         case 1:                                                                            /* a conversão de reias para a moeda selecionada, colocando um if para verificar*/
-            printf("Voce converteu %.2f reias em %.2f BTC.",valor_usu, valor_usu/btc);     /* se a moeda realmente se encaixa no que foi pedido.*/
+            printf("Voce converteu %.2f reias em %.2f BTC.\n",valor_usu, valor_usu/btc);     /* se a moeda realmente se encaixa no que foi pedido.*/
             break;
         case 2:
-            printf("Voce converteu %.2f reias em %.2f USD.",valor_usu, valor_usu/usd);
+            printf("Voce converteu %.2f reias em %.2f USD.\n",valor_usu, valor_usu/usd);
             break;
         case 3:
-            printf("Voce converteu %.2f reias em %.2f GBP.",valor_usu, valor_usu/gbp);
+            printf("Voce converteu %.2f reias em %.2f GBP.\n",valor_usu, valor_usu/gbp);
             break;
         case 4:
-            printf("Voce converteu %.2f reias em %.2f EUR.",valor_usu, valor_usu/eur);
+            printf("Voce converteu %.2f reias em %.2f EUR.\n",valor_usu, valor_usu/eur);
             break;
         case 5:
-            printf("Voce converteu %.2f reias em %.2f JPY.",valor_usu, valor_usu/jpy);
+            printf("Voce converteu %.2f reias em %.2f JPY.\n",valor_usu, valor_usu/jpy);
             break;
         default:
-            printf("Erro ao escolher moeda!!");
+            printf("Erro ao escolher moeda!!\n");
 
-    if (moeda < 0 || moeda > 5) {
-        printf("Erro ao escolher moeda!!");
-    }}
+    }
     return 0;
 }
